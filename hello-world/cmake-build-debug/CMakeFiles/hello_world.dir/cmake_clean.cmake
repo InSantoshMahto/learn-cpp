2f3359e38a3bc2da628784473b9d5e6a1d24312b
santosh mahto <mahtos480@gmail.com>
2020-04-28 10:23:58 +0530

    learning

---
+++ hello-world/cmake-build-debug/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/main.cpp.o"
+  "hello_world"
+  "hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
