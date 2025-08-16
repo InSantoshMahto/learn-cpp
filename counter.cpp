1b9e05913668215f0c40e6412c9b41aa4d2bbc66
Santosh Mahto <insantoshmahto@icloud.com>
2025-08-16 19:33:33 +0530

    Add counter to track consecutive input occurrences

--- counter.cpp
+++ counter.cpp
@@ -2,6 +2,23 @@
 using namespace std;
 int main()
 {
-	cout << "Hello, World!" << endl;
+	cout << "Count Number of Consecutive Inputs" << endl;
+	uint currVal = 0, val = 0;
+	if (cin >> currVal) {
+		uint count = 1;
+		while (cin >> val) {
+			if (val == currVal) {
+				++count;
+			} else {
+				cout << currVal << " occurs " << count << " times." << endl;
+				currVal = val;
+				count = 1;
+			}
+			// To break this loop, you can use 'break;' statement
+			// or input EOF (Ctrl+D on Unix/Linux/Mac or Ctrl+Z on Windows)
+			// or redirect input from a file that ends
+		}
+		cout << currVal << " occurs " << count << " times." << endl;
+	}
 	return 0;
 }
